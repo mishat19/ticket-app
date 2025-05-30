861d29f535ddbbc04f98a7373a00270fb731900c
Mathis Brongniart <spidarstudiogames@gmail.com>
2025-05-30 23:44:21 +0200

    Ajout fichier JSON et modif fichiers

---
+++ lib/base/utils/all_json.dart
@@ -0,0 +1,30 @@
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from':{ //String
+      'code': "NYC", //Dynamic
+      'name': "New-York"
+    },
+    'to':{
+      'code': "LDN",
+      'name': "London"
+    },
+    'flying_time': '8H 30M',
+    'date': "1 MAY",
+    'departure_time': "08:00 AM",
+    'number': 23, //Dynamic
+  }, //Map
+  {
+    'from':{ //String
+      'code': "PAR", //Dynamic
+      'name': "Paris"
+    },
+    'to':{
+      'code': "SAN",
+      'name': "Shanghai"
+    },
+    'flying_time': '13H 30M',
+    'date': "30 MAY",
+    'departure_time': "09:00 PM",
+    'number': 7, //Dynamic
+  },
+]; //List
